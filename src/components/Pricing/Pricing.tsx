1fdf32fddfe5d0f6080162166b9aeaed9d078976
Ortannu <79311050+Ortannu@users.noreply.github.com>
2025-04-15 14:48:41 +0200

    Update Pricing.tsx

--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -1,15 +1,18 @@
 import PricingColumn from "./PricingColumn";
-
 import { tiers } from "@/data/pricing";
 
 const Pricing: React.FC = () => {
-    return (
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-            {tiers.map((tier, index) => (
-                <PricingColumn key={tier.name} tier={tier} highlight={index === 1} />
-            ))}
-        </div>
-    )
-}
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+      {tiers.map((tier, index) => (
+        <PricingColumn
+          key={tier.name}
+          tier={tier}
+          highlight={index === 1} // evidenzia il piano "Pro"
+        />
+      ))}
+    </div>
+  );
+};
 
-export default Pricing
+export default Pricing;
