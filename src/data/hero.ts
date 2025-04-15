d1bd7cba7996a285b64a53942b4e99157bbfe342
Ortannu <79311050+Ortannu@users.noreply.github.com>
2025-04-15 14:40:59 +0200

    Update hero.ts

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,6 @@
 export const heroDetails = {
-    heading: 'Smart, Secure, Simple Financial Management',
-    subheading: 'From effortless budgeting to real-time investment insights, Finwise puts you in control of your money like never before',
-    centerImageSrc: '/images/hero-mockup.webp',
-}
+  heading: 'Multilingual AI Chatbot for Ecommerce',
+  subheading:
+    'Launch an intelligent chatbot in 3 minutes. Save time, support globally, and track everything in real-time.',
+  centerImageSrc: '/images/hero-ai.webp',
+};
