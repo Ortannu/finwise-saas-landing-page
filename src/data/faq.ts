485a1e430882d9ead625f6d4a61ba75f7d58655e
Ortannu <79311050+Ortannu@users.noreply.github.com>
2025-04-15 14:40:18 +0200

    Update faq.ts

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -2,24 +2,25 @@ import { IFAQ } from "@/types";
 import { siteDetails } from "./siteDetails";
 
 export const faqs: IFAQ[] = [
-    {
-        question: `Is ${siteDetails.siteName} secure?`,
-        answer: 'Absolutely. We use bank-level encryption to protect your data and never store your login information. Plus, our biometric authentication adds an extra layer of security.',
-    },
-    {
-        question: `Can I use ${siteDetails.siteName} on multiple devices?`,
-        answer: 'Absolutely! Your Finwise account syncs seamlessly across all your devices - smartphone, tablet, and computer.',
-    },
-    {
-        question: 'Can I connect all my bank accounts?',
-        answer: `Yes! ${siteDetails.siteName} supports connections with over 10,000 financial institutions worldwide. Simply log in through our secure portal to link your accounts.`
-    },
-    {
-        question: 'Do I need any financial expertise to use the investing features?',
-        answer: 'Not at all! Our expert-curated portfolios and educational resources make investing accessible to everyone, regardless of experience level.',
-    },
-    {
-        question: 'What if I need help using the app?',
-        answer: 'Our dedicated support team is available 24/7 via chat or email. Plus, we offer extensive in-app tutorials and a comprehensive knowledge base to help you make the most of Finwise.'
-    }
+  {
+    question: `Does ${siteDetails.siteName} support multiple languages?`,
+    answer:
+      'Yes, our AI chatbot supports 100+ languages using Google Translate API.',
+  },
+  {
+    question: 'Can I integrate it in my Shopify store?',
+    answer: 'Absolutely. You can paste one line of code into your Shopify theme.',
+  },
+  {
+    question: 'Can I track my leads and messages?',
+    answer: 'Yes, everything is tracked in your real-time dashboard.',
+  },
+  {
+    question: 'Is setup difficult?',
+    answer: 'No dev skills needed. You can launch in 3 minutes.',
+  },
+  {
+    question: 'Do you offer a free trial?',
+    answer: 'Yes. Every plan comes with a 3-day free trial and full access.',
+  },
 ];
