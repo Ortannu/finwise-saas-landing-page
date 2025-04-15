ebf789bee6a3c245c63c3b0d2ad1ed2b3e0aaf93
Ortannu <79311050+Ortannu@users.noreply.github.com>
2025-04-15 14:49:18 +0200

    Update Benefits.tsx

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -1,16 +1,15 @@
-import BenefitSection from "./BenefitSection"
-
-import { benefits } from "@/data/benefits"
+import BenefitSection from "./BenefitSection";
+import { benefits } from "@/data/benefits";
 
 const Benefits: React.FC = () => {
-    return (
-        <div id="features">
-            <h2 className="sr-only">Features</h2>
-            {benefits.map((item, index) => {
-                return <BenefitSection key={index} benefit={item} imageAtRight={index % 2 !== 0} />
-            })}
-        </div>
-    )
-}
+  return (
+    <section id="features" className="py-10 lg:py-20">
+      <h2 className="sr-only">Features</h2>
+      {benefits.map((item, index) => (
+        <BenefitSection key={index} benefit={item} imageAtRight={index % 2 !== 0} />
+      ))}
+    </section>
+  );
+};
 
-export default Benefits
+export default Benefits;
